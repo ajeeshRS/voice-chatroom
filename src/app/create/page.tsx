ebe819c8e81fdde2cf53cb4a9fb76c664244aa9c
ajeeshRS <ajeeshrs79@gmail.com>
2025-01-31 15:54:16 +0530

    udpate join and create pages to be mobile responsive

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,12 +1,12 @@
 export default function Page() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-2/6 h-2/6 flex flex-col items-center justify-start p-10 rounded-xl shadow-md space-y-3">
+      <div className="md:w-2/6 w-5/6 h-2/6 flex flex-col items-center justify-start p-10 rounded-xl shadow-md space-y-3">
         <h4 className="font-bold text-xl">Create New Room</h4>
         <input
           type="text"
           placeholder="Enter room name"
-          className="p-3 rounded-md border w-5/6 focus:outline-none"
+          className="p-3 rounded-md border md:w-5/6 w-full focus:outline-none"
         />
         <button className="bg-black text-white px-4 py-2 rounded-md hover:bg-neutral-800">
           create
--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,11 +1,11 @@
 export default function Page() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-2/6 h-2/6 flex flex-col items-center justify-start p-10 rounded-xl shadow-md space-y-3">
+      <div className="md:w-2/6 w-5/6 h-2/6 flex flex-col items-center justify-start p-10 rounded-xl shadow-md space-y-3">
         <h4 className="font-bold text-xl">Enter Room ID</h4>
         <input
           type="text"
-          className="p-3 rounded-md border w-5/6 focus:outline-none"
+          className="p-3 rounded-md border md:w-5/6 w-full focus:outline-none"
         />
         <button className="bg-black text-white px-4 py-2 rounded-md hover:bg-neutral-800">
           Join
