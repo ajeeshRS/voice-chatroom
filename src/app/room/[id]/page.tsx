e4c55677ed153b30a3ae87f7e9dff017c6cf842e
ajeeshRS <ajeeshrs79@gmail.com>
2025-01-31 16:12:37 +0530

    update room page to be mobile responsive

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -4,21 +4,26 @@ import { AudioLines, Hand, LogOut, Mic, MicOff } from "lucide-react";
 export default function Page() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-5/6 h-5/6 border rounded-2xl py-20 px-10 flex items-start justify-between">
-        <div className="w-5/6 h-full grid grid-cols-4 px-5 gap-5 items-start overflow-y-scroll ">
+      <div className="md:w-5/6 w-full h-5/6 md:border rounded-2xl md:py-20 py-10 px-10 flex md:flex-row flex-col items-start justify-between">
+        {/* participants */}
+        <div className="md:w-5/6 w-full h-full grid md:grid-cols-4 grid-cols-3 md:px-5 md:pb-0 pb-4 gap-5 items-start overflow-y-scroll ">
           {names.map((name, i) => (
-            <div key={i} className="flex flex-col items-center space-y-1 ">
+            <div
+              key={i}
+              className="flex flex-col items-center text-center space-y-1 "
+            >
               <p className="relative rounded-full w-10 h-10 flex items-center justify-center px-2 p-3 bg-black text-white">
                 {name.charAt(0).toUpperCase()}
                 {/* <MicOff className="w-4 h-4 absolute -bottom-1 -right-3 text-purple-500" /> */}
                 {/* <AudioLines className="w-4 h-4 absolute -bottom-1 -right-3 text-purple-500" /> */}
               </p>
-              <p className="text-neutra-800 text-xs">{name}</p>
+              <p className="text-neutra-800 md:text-xs text-[10px]">{name}</p>
             </div>
           ))}
         </div>
-        <div className="w-1/4 h-full flex flex-col items-start justify-between pb-10 space-y-2 px-5 border-l">
-          <div className="flex flex-col items-start space-y-2">
+        {/* room info and controls */}
+        <div className="md:w-1/4 w-full h-full flex flex-col items-start justify-between pb-10 space-y-2 px-5 md:border-l border-t md:border-t-0">
+          <div className="flex flex-col items-start space-y-2 md:my-0 my-5">
             <h4 className="text-4xl font-bold text-neutral-800">Motohub</h4>
             <p className="text-neutral-800 font-semibold text-xs">
               #45234t23-43534bhjb-dsf
