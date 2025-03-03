3ad39ad2df8c1d5346f59099c351d6ba05eafec1
ajeeshRS <ajeeshrs79@gmail.com>
2025-03-03 20:00:28 +0530

    added proper loader

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { names } from "@/app/utils/data";
 import { useSocket } from "@/hooks/useSocket";
-import { AudioLines, Hand, LogOut, Mic, MicOff } from "lucide-react";
+import { AudioLines, Hand, Loader, LogOut, Mic, MicOff } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -47,15 +47,20 @@ export default function Page() {
       // hanlding on message events
       socket.addEventListener("message", (e) => {
         const parsedData = JSON.parse(e.data);
-        console.log(parsedData);
+        console.log("parsed data - msg event listener : ", parsedData);
+
         if (parsedData.type === "room_details") {
           setUserCount(parsedData.participantCount);
           setUsers(parsedData.users);
-          console.log(parsedData.users);
         } else if (parsedData.type === "left") {
-          console.log("left the room reached");
           router.push("/");
           toast.success("Left the room");
+        } else if (parsedData.type === "user_left") {
+          console.log("user left msg");
+          toast.success(parsedData.message);
+        } else if (parsedData.type === "user_joined") {
+          toast.success("new user joined the room");
+          console.log("new user joined");
         }
       });
     } else {
@@ -71,16 +76,21 @@ export default function Page() {
     };
   }, [socket, id]);
 
-  if (loading) return <div>loading..</div>;
+  if (loading)
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        <Loader className="w-5 h-5 animate-spin origin-center"/>
+      </div>
+    );
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="md:w-5/6 w-full h-5/6 md:border rounded-2xl md:py-20 py-10 px-10 flex md:flex-row flex-col items-start justify-between">
         {/* participants */}
         <div className="md:w-5/6 w-full h-full grid md:grid-cols-4 grid-cols-3 md:px-5 md:pb-0 pb-4 gap-5 items-start overflow-y-scroll ">
-          {users.map((user) => (
+          {users.map((user, i) => (
             <div
-              key={user.username}
+              key={i}
               className="flex flex-col items-center text-center space-y-1 "
             >
               <p className="relative rounded-full w-10 h-10 flex items-center justify-center px-2 p-3 bg-black text-white">
